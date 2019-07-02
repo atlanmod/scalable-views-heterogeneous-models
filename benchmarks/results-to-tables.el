75d2b358e065743da70a99483c9b30c82e3a832d
fmdkdd <fmdkdd@0xc0de.fr>
2019-07-02 18:27:58 +0200

    Refactor + test out boxplots

--- benchmarks/results-to-tables.el
+++ benchmarks/results-to-tables.el
@@ -1,39 +1,70 @@
-(defun string-mean-sdev (line)
-  (let* ((l (split-string line ": "))
-         (md (split-string
-              (calc-eval '("vmean($), vsdev($)" calc-float-format (fix 0)) nil (cadr l))
-              ", ")))
-    (list (cons 'task (car l))
-          (cons 'mean (car md))
-          (cons 'sdev (cadr md)))))
-
-(string-mean-sdev "Load view: [172,179,170,166,172,168,166,175,166,168]")
-((task . "Load view") (mean . "170.") (sdev . "4."))
-
-(defun task-mean-sdev (bench task)
-  (with-current-buffer "all-xeon"
+(defun task-line (buffer bench task)
+  (with-current-buffer buffer
     (goto-char (point-min))
     (re-search-forward (format "^# Results for: %s$" bench))
     (re-search-forward (format "^%s" task))
-    (string-mean-sdev
-     (buffer-substring (line-beginning-position) (line-end-position)))))
+    (buffer-substring (line-beginning-position) (line-end-position))))
 
-(task-mean-sdev "Load view with XMI trace of size 10" "Load view")
+(task-line "all-xeon" "Load view with XMI trace of size 10" "Load view")
+"Load view: [169,168,155,163,165,153,164,163,151,163]"
+
+(defun parse-task-line (line)
+  (split-string line ": "))
+
+(parse-task-line "Load view: [169,168,155,163,165,153,164,163,151,163]")
+("Load view" "[169,168,155,163,165,153,164,163,151,163]")
+
+(defun task-mean-sdev (task)
+  (let ((md (split-string
+             (calc-eval '("vmean($), vsdev($)" calc-float-format (fix 0)) nil (cadr task))
+             ", ")))
+    (list (cons 'task (car task))
+          (cons 'mean (car md))
+          (cons 'sdev (cadr md)))))
+
+(task-mean-sdev '("Load view" "[172,179,170,166,172,168,166,175,166,168]"))
 ((task . "Load view") (mean . "170.") (sdev . "4."))
 
-(task-mean-sdev "Load view with XMI trace of size 10" "Get all")
-((task . "Get all elements") (mean . "577.") (sdev . "10."))
+(task-mean-sdev
+ (parse-task-line
+  (task-line "all-xeon" "Load view with XMI trace of size 10" "Load view")))
+((task . "Load view") (mean . "161.") (sdev . "6."))
 
-(defun bench-task-mean-sdev (bench task sizes &optional suffix)
+(task-mean-sdev
+ (parse-task-line
+  (task-line "all-xeon" "Load view with XMI trace of size 10" "Get all")))
+((task . "Get all elements") (mean . "595.") (sdev . "5."))
+
+(defun bench-tasks (buffer bench task sizes &optional suffix)
   (mapcar
    (lambda (s)
      (cons (cons 'size s)
-           (task-mean-sdev (format "%s of size %d%s" bench s (or suffix "")) task)))
+           (parse-task-line
+            (task-line buffer (format "%s of size %d%s" bench s (or suffix "")) task))))
    sizes))
 
-(bench-task-mean-sdev "Load view with XMI trace" "Load view" '(10 100))
-(((size . 10) (task . "Load view") (mean . "170.") (sdev . "4."))
- ((size . 100) (task . "Load view") (mean . "167.") (sdev . "4.")))
+(bench-tasks "all-xeon" "Load view with XMI trace" "Load view" '(10 100))
+(((size . 10) "Load view" "[169,168,155,163,165,153,164,163,151,163]")
+ ((size . 100) "Load view" "[163,153,165,164,155,164,163,154,165,152]"))
+
+(defun bench-task-mean-sdev (bench task sizes &optional suffix)
+  (mapcar
+   (lambda (tsk) (cons (car tsk) (task-mean-sdev (cdr tsk))))
+   (bench-tasks "all-xeon" bench task sizes suffix)))
+
+(mapcar
+ (lambda (tsk) (cons (car tsk)
+                     (task-mean-sdev (cdr tsk))))
+ (bench-tasks "all-xeon" "Load view with XMI trace" "Load view" '(10 100 1000)))
+(((size . 10)   (task . "Load view") (mean . "161.") (sdev . "6."))
+ ((size . 100)  (task . "Load view") (mean . "160.") (sdev . "6."))
+ ((size . 1000) (task . "Load view") (mean . "191.") (sdev . "6.")))
+
+(bench-task-mean-sdev "Load view with XMI trace" "Load view" '(10 100 1000))
+(((size . 10)   (task . "Load view") (mean . "161.") (sdev . "6."))
+ ((size . 100)  (task . "Load view") (mean . "160.") (sdev . "6."))
+ ((size . 1000) (task . "Load view") (mean . "191.") (sdev . "6.")))
+
 
 (defun print-table (results)
   (insert (format "| Size | %s |\n" (alist-get 'task (car results))))
@@ -80,3 +111,79 @@ EOL query traceToReqs.eol for NeoEMF view with EMF EMC
 | 4 | 7816. ± 158. |
 | 5 | 23494 ± 1086. |
 | 6 | 179842. ± 3111. |
+
+
+;; Outputting data to gnuplot to see if boxplots are worth it
+
+(task-line "all-xeon" "Load view with XMI trace of size 10" "Load view")
+"Load view: [169,168,155,163,165,153,164,163,151,163]"
+
+(defun vector-string-to-list (str)
+  (cdr (calc-eval str 'raw)))
+
+(vector-string-to-list
+ (cdr (parse-task-line (task-line "all-xeon" "Load view with XMI trace of size 10" "Load view"))))
+(169 168 155 163 165 153 164 163 151 163)
+
+(bench-tasks "all-xeon" "OCL query 0 for full view on XMI trace" "Evaluate query" '(10 100 1000))
+(((size . 10) "Evaluate query" "[612,614,610,647,609,606,613,607,615,642]")
+ ((size . 100) "Evaluate query" "[604,609,612,612,611,647,611,613,611,610]")
+ ((size . 1000) "Evaluate query" "[619,623,647,614,612,614,620,622,646,615]"))
+
+(defun bench-task-raw (bench bench-name task sizes &optional suffix)
+  (mapcar
+   (lambda (tsk)
+     (list (cons 'bench bench-name)
+           (cons 'task (nth 1 tsk))
+           (nth 0 tsk)
+           (cons 'values (vector-string-to-list (nth 2 tsk)))))
+   (bench-tasks "all-xeon" bench task sizes suffix)))
+
+(bench-task-raw "OCL query 0 for full view on XMI trace" "xmi" "Evaluate query" '(10 100 1000))
+(((bench . "xmi") (task . "Evaluate query") (size . 10) (values 612 614 610 647 609 606 613 607 615 642))
+ ((bench . "xmi") (task . "Evaluate query") (size . 100) (values 604 609 612 612 611 647 611 613 611 610))
+ ((bench . "xmi") (task . "Evaluate query") (size . 1000) (values 619 623 647 614 612 614 620 622 646 615)))
+
+(defun compare-benches (task benches &optional sizes)
+  (-mapcat
+   (lambda (b)
+     (bench-task-raw (nth 0 b) (nth 1 b) task (or sizes '(10 100 1000 10000 100000 1000000)) (nth 2 b)))
+   benches))
+
+(compare-benches "Evaluate query"
+                 '(("OCL query 0 for full view on XMI trace" "xmi")
+                   ("OCL query 0 for full view on XMI trace" "xmi-fast" " with fast extents map"))
+                 '(10 100))
+(((bench . "xmi") (task . "Evaluate query") (size . 10) (values 612 614 610 647 609 606 613 607 615 642))
+ ((bench . "xmi") (task . "Evaluate query") (size . 100) (values 604 609 612 612 611 647 611 613 611 610))
+ ((bench . "xmi-fast") (task . "Evaluate query") (size . 10) (values 10 11 11 10 11 10 10 10 10 10))
+ ((bench . "xmi-fast") (task . "Evaluate query") (size . 100) (values 10 10 11 10 10 9 11 9 10 10)))
+
+(defun print-gnuplot (results)
+  (dolist (r results)
+    (dolist (x (alist-get 'values r))
+      (insert (format "%d %d \"%s\"\n" (log10 (alist-get 'size r)) x (alist-get 'bench r))))))
+
+(print-gnuplot
+ (bench-task-raw "OCL query 0 for full view on XMI trace" "xmi" "Evaluate query" '(10 100)))
+
+1 612 "xmi"
+1 614 "xmi"
+1 610 "xmi"
+1 647 "xmi"
+1 609 "xmi"
+1 606 "xmi"
+1 613 "xmi"
+1 607 "xmi"
+1 615 "xmi"
+1 642 "xmi"
+2 604 "xmi"
+2 609 "xmi"
+2 612 "xmi"
+2 612 "xmi"
+2 611 "xmi"
+2 647 "xmi"
+2 611 "xmi"
+2 613 "xmi"
+2 611 "xmi"
+2 610 "xmi"
